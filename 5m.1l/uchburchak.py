e9c4a198d9edbbe8428ecf2e2f4b571aef2ca453
Afzal <krisalya4@gmail.com>
2025-03-13 23:14:57 +0500

    bug fix ;)

--- 5m.1l/uchburchak.py
+++ 5m.1l/uchburchak.py
@@ -1,12 +1,17 @@
 n=int(input())
 l1=list(map(int,input().split()))
 
-def uchburchak(l :list):
-    l.sort()
-    a=l[-3]
-    b=l[-2]
-    c=l[-1]
+def uchburchak(l :list,i: int=0):
+    a=l[-3+i]
+    b=l[-2+i]
+    c=l[-1+i]
     if a+b>c and a+c>b and b+c>a:
-        return a,b,c
-    else:
+        return f'{a} {b} {c}'
+    elif len(l)==3+(-i):
         return -1
+    else:
+        i-=1
+        return uchburchak(l,i)
+        
+
+print(uchburchak(sorted(l1)))
