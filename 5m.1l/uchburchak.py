c66c1cf51d4f42d17e766384cbac2cf28692641d
Afzal <krisalya4@gmail.com>
2025-03-13 23:22:52 +0500

    a little optimization for uchburchak.py
    i added n for a function so i doesn't have to measure the length of a list, it' already measured and i named it as 'n'.

--- 5m.1l/uchburchak.py
+++ 5m.1l/uchburchak.py
@@ -1,17 +1,17 @@
 n=int(input())
 l1=list(map(int,input().split()))
 
-def uchburchak(l :list,i: int=0):
+def uchburchak(l :list,n:int,i: int=0):
     a=l[-3+i]
     b=l[-2+i]
     c=l[-1+i]
     if a+b>c and a+c>b and b+c>a:
         return f'{a} {b} {c}'
-    elif len(l)==3+(-i):
+    elif n==3+(-i):
         return -1
     else:
         i-=1
-        return uchburchak(l,i)
+        return uchburchak(l,n,i)
         
 
-print(uchburchak(sorted(l1)))
+print(uchburchak(sorted(l1),n))
